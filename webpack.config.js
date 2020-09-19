da96046f3c1d1707b325436a931e1962a29d79fa
Luke Swan <swan.luke9@gmail.com>
2020-09-19 13:08:47 +0100

    added htmlwebpackplugin to save us the need of having an html file. Removed dist folder - output goes to build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+ mode: 'development',
+ devtool: 'eval',
   entry: "./src/main.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
   },
+  plugins:[
+    new HtmlWebpackPlugin()
+  ]
 };
